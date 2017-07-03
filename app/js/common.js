3d7c5a4b7d3debfa1d3a03698070ffd2275db651
OlegMarchenko <workolegmarchenko@gmail.com>
2017-07-03 13:11:16 +0300

    changes

--- app/js/common.js
+++ app/js/common.js
@@ -3,4 +3,11 @@ $(function(){
         dots: true,
         arrows: false
     });
+    $(".navbar-toggler").click(function(){
+        $(".navbar-nav").slideToggle(function(){
+            if($(this).css('display') === 'none'){
+                $(this).removeAttr('style');
+            }
+        });
+    })
 });
